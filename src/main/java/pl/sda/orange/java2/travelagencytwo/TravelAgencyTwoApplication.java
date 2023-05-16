c6435773ac8fb8a745cb89321a754ce331e61065
Lesio <e.leszek.one@gmail.com>
2023-05-16 20:43:04 +0200

    --initial commit

---
+++ src/main/java/pl/sda/orange/java2/travelagencytwo/TravelAgencyTwoApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.orange.java2.travelagencytwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/orange/java2/travelagencytwo/TravelAgencyTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.orange.java2.travelagencytwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
