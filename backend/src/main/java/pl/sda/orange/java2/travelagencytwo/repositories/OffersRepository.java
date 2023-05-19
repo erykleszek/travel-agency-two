9e7dce01a9626788a77dbc8d2608aa6d9df1aa14
gosia <wheresmyredpillmorpheus@gmail.com>
2023-05-19 23:13:36 +0200

    security partially user and registration /backend

--- backend/src/main/java/pl/sda/orange/java2/travelagencytwo/repositories/OffersRepository.java
+++ backend/src/main/java/pl/sda/orange/java2/travelagencytwo/repositories/OffersRepository.java
@@ -1,8 +1,8 @@
-package pl.sda.orange.java2.repositories;
+package pl.sda.orange.java2.travelagencytwo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.sda.orange.java2.entities.Offers;
+import pl.sda.orange.java2.travelagencytwo.entities.Offers;
 
 @Repository
 public interface OffersRepository extends JpaRepository<Offers,Long> {
---
+++ backend/src/main/java/pl/sda/orange/java2/travelagencytwo/security/PasswordEncoder.java
@@ -0,0 +1,15 @@
+package pl.sda.orange.java2.travelagencytwo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
