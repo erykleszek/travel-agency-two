1e24a71eade2e6680ac80efe98a2ec79f7c53b85
Lesio <e.leszek.one@gmail.com>
2023-05-25 20:25:47 +0200

    --entity Trip

--- backend/src/main/java/pl/sda/orange/java2/travelagencytwo/user/token/ConfirmationToken.java
+++ backend/src/main/java/pl/sda/orange/java2/travelagencytwo/user/token/ConfirmationToken.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class ConfirmationToken {
     @Id
     @SequenceGenerator(name = "confirmation_token_sequence",
