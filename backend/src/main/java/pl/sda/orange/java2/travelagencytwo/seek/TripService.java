d384cf03f3c8dbf11f21e147128563016031eaf2
Lesio <e.leszek.one@gmail.com>
2023-06-03 11:23:03 +0200

    -create TripDTO

--- backend/src/main/java/pl/sda/orange/java2/travelagencytwo/seek/TripService.java
+++ backend/src/main/java/pl/sda/orange/java2/travelagencytwo/seek/TripService.java
@@ -1,15 +1,20 @@
 package pl.sda.orange.java2.travelagencytwo.seek;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class TripService {
     @Autowired
     private TripRepository tripRepository;
+
 
     public List<Trip> searchTrips(String departureCity, String destinationCity, LocalDate departureDateStart,
                                   LocalDate departureDateEnd, LocalDate returnDateStart, LocalDate returnDateEnd, String type,
